2de3ace8da61d9fe2e67a98b5a3bbbbca0be6fe3
David Gunyon <davidgunyon84@gmail.com>
2017-08-07 10:58:10 -0400

    Bank App Starting Point

---
+++ src/data/users.js
@@ -0,0 +1,135 @@
+const userList = () => {
+  return [
+     {
+        _id:"597f7521f2dbcd7664363339",
+        name:"Wade Wilson",
+        email:"stalkmeplease@aol.com",
+        phone:"+1 (555) 555.5555",
+        address:"Canadia",
+        accounts:[
+           {
+              id:0,
+              accountType:"checking",
+              balance:326067981.14
+           },
+           {
+              id:2,
+              accountType:"credit",
+              balance:193872109836.46
+           }
+        ]
+     },
+     {
+        _id:"597f752152b81f549f6e7448",
+        name:"Dinah Lance",
+        email:"canary.call@gmail.com",
+        phone:"+1 (555) 555.5556",
+        address:"505 Tiffany Place, Fresno, Iowa, 5532",
+        accounts:[
+           {
+              id:0,
+              accountType:"checking",
+              balance:85.05
+           },
+           {
+              id:1,
+              accountType:"savings",
+              balance:391.11
+           },
+           {
+              id:2,
+              accountType:"credit",
+              balance:10000.00
+           }
+        ]
+     },
+     {
+        _id:"597f7521d0a7a70da9c27922",
+        name:"Caitlyn Snow",
+        email:"frosty.snow@starlabs.com",
+        phone:"+1 (555) 555-5557",
+        address:"Star City",
+        accounts:[
+           {
+              id:0,
+              accountType:"checking",
+              balance:11920.32
+           },
+           {
+              id:2,
+              accountType:"credit",
+              balance:25499.69
+           }
+        ]
+     },
+     {
+        _id:"597f7521a043e7b3242b2448",
+        name:"Oliver Queen",
+        email:"billionairequeen@queenconsolidated.com",
+        phone:"+1 (555) 555-5558",
+        address:"Star City",
+        accounts:[
+           {
+              id:0,
+              accountType:"checking",
+              balance:10444678.08
+           },
+           {
+              id:1,
+              accountType:"savings",
+              balance:28000689.33
+           }
+        ]
+     },
+     {
+        _id:"597f7521133389c4c6fc39c4",
+        name:"Felicity Smoak",
+        email:"smoaking.hacker@queenconsolidated.com",
+        phone:"+1 (555) 555-5559",
+        address:"Star City",
+        accounts:[
+           {
+              id:0,
+              accountType:"checking",
+              balance:245000.46
+           },
+           {
+              id:1,
+              accountType:"savings",
+              balance:1746300.00
+           },
+           {
+              id:2,
+              accountType:"credit",
+              balance:13000000.00
+           }
+        ]
+     },
+     {
+        _id:"597f75211e7743a87e2003ea",
+        name:"Barry Allen",
+        email:"ballen@starlabs.com",
+        phone:"+1 (555) 555-5560",
+        address:"946 Summit Street, Kapowsin, American Samoa, 5865",
+        accounts:[
+           {
+              id:0,
+              accountType:"checking",
+              balance:86.08
+           },
+           {
+              id:1,
+              accountType:"savings",
+              balance:100.00
+           },
+           {
+              id:2,
+              accountType:"credit",
+              balance:5000.70
+           }
+        ]
+     }
+  ];
+}
+
+export default userList;
